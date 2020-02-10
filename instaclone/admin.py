3d15b41b6882309182aa08d2eade31776d29ccec
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-10 15:20:48 +0300

    created the admin file

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Post,Comment,Location,Like,Follow
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Location)
+admin.site.register(Like)
+admin.site.register(Follow)
