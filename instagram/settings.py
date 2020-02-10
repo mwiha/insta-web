744510f8fa3e2cca28a98ef9b4ed29b1865331f2
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-10 11:44:27 +0300

    created base.html in templates

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'instaclone',
+    # 'instaclone',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instaclone.apps.InstacloneConfig',
 ]
 
 MIDDLEWARE = [
